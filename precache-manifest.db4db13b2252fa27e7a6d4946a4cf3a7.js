75bc53732bb0eb894f0b471e3c0decde95225670
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-09 14:40:59 +0300

    Updates

---
+++ precache-manifest.db4db13b2252fa27e7a6d4946a4cf3a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499c1f881ebd2aab5edf5c66eeb09677",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "2d4d1f543479bffc3640",
+    "url": "/goit-react-hw-02-movies/static/css/main.15f69962.chunk.css"
+  },
+  {
+    "revision": "f2be40fa08f548757245",
+    "url": "/goit-react-hw-02-movies/static/js/2.07ba98d0.chunk.js"
+  },
+  {
+    "revision": "2d4d1f543479bffc3640",
+    "url": "/goit-react-hw-02-movies/static/js/main.3d1f75d7.chunk.js"
+  },
+  {
+    "revision": "0069d2c8e0e7d1c56d0e",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.58f451ba.js"
+  }
+]);
